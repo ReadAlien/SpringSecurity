d3cb2435d07c820474a69928f005e991876e6b39
Cro <junsj1230@naver.com>
2024-06-04 21:28:41 +0900

    JdbcUserDetailsManager 및 UserDetailsService 직접 구현한 로그인

--- springsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ springsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -6,10 +6,14 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
@@ -49,22 +53,35 @@ public class ProjectSecurityConfig {
     /**
      * InMemory 방식 2
      */
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService() {
+//
+//        /**
+//         * User. 후에 첫 번째 체이닝에는 항상 withUsername 이나 withDefaultPasswordEncoder 만 가능하다
+//         */
+//        UserDetails admin = User.withUsername("admin")
+//                .password("12345")
+//                .authorities("admin")
+//                .build();
+//
+//        UserDetails user = User.withUsername("user")
+//                .password("12345")
+//                .authorities("read")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
 
-        UserDetails admin = User.withUsername("admin")
-                .password("12345")
-                .authorities("admin")
-                .build();
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource) {
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
 
-        UserDetails user = User.withUsername("user")
-                .password("12345")
-                .authorities("read")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, user);
-    }
-
+    /**
+     * NoOpPasswordEncoder 방식은 실무용으로 적합하지 않으니
+     * 실습할 때만 사용 권장
+     * @return PasswordEncoder
+     */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
