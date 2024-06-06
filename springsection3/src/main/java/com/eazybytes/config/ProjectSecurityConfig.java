e89a818217b976b1287b6ee98a1677334fb165b8
Cro <junsj1230@naver.com>
2024-06-07 01:54:04 +0900

    PasswordEncoder 변경

--- springsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ springsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -4,13 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
@@ -21,9 +16,10 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests(authorize -> authorize
+        http.csrf().disable()
+        .authorizeRequests(authorize -> authorize
                         .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCard").authenticated()
-                        .requestMatchers("/notices", "/contact").permitAll())
+                        .requestMatchers("/notices", "/contact", "/register").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
